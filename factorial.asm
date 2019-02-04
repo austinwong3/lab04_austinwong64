5b863425defaa7a533f94f21a436640e3fd8de8b
Austin Wong <austinwong@umail.ucsb.edu>
2019-02-04 00:40:29 -0800

    factorial test

--- factorial.asm
+++ factorial.asm
@@ -24,8 +24,43 @@ newline:
 .text
 
 main:
+	li $v0, 4
+	la $a0, prompt
+	syscall
 
-	# TODO: Write your code here
+	li $v0, 5
+	move $t0, $v0
+	syscall
+
+	li $t1, 1
+
+	loop:
+		beq $t0, $zero, print
+		mult $t1, $t0
+		mflo $t1
+
+		li $v0, 1
+		move $a0, $t1
+		syscall
+
+		li $v0, 4
+		la $a0, newline
+		syscall
+
+		sub $t0, $t0, 1
+
+		j loop
+
+	print:
+		li $v0, 1
+		move $a0, $t1
+		syscall
+
+		li $v0, 4
+		la $a0, newline
+		syscall
+
+
 
 exit:
 
